d61e2b237fcae12b6851b5082dc6e068cfbe831e
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2024-05-01 01:53:27 +0600

    Added contacts

--- src/script.js
+++ src/script.js
@@ -1,1 +1,38 @@
 import './style.css';
+
+const preloadImage = new Image();
+preloadImage.src = './assets/images/double-chevron-down.gif';
+
+const contacts = document.getElementById('contacts');
+const contactsLinks = document.getElementById('contacts-links');
+const contactsIcon = document.getElementById('contacts-icon');
+const contactsRings = document.getElementById('contacts-rings');
+
+contacts.addEventListener('click', () => {
+  const isDisplayFlex = contactsLinks.style.display === 'flex';
+
+  contactsLinks.classList.remove(!isDisplayFlex ? 'animate-translate-bottom' : 'animate-translate-top');
+  contactsLinks.classList.add(isDisplayFlex ? 'animate-translate-bottom' : 'animate-translate-top');
+
+  const switchContacts = () => {
+    contactsIcon.src = !isDisplayFlex ? './assets/images/double-chevron-down.gif' : './assets/images/contacts.png';
+    contactsRings.style.display = isDisplayFlex ? 'initial' : 'none';
+    contactsLinks.style.display = isDisplayFlex ? 'none' : 'flex';
+  };
+
+  if (isDisplayFlex) {
+    setTimeout(switchContacts, 250);
+  } else {
+    switchContacts();
+  }
+});
+
+const header = document.getElementsByTagName('header')[0];
+
+document.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    header.classList.add('header-scrolled');
+  } else {
+    header.classList.remove('header-scrolled');
+  }
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'translate-top': {
+          '0%': { transform: 'translateY(13rem)' },
+          '100%': { transform: 'translateY(0)' },
+        },
+        'translate-bottom': {
+          '0%': { transform: 'translateY(0)' },
+          '100%': { transform: 'translateY(13rem)' },
+        },
+      },
+      animation: {
+        'translate-top': 'translate-top 250ms linear',
+        'translate-bottom': 'translate-bottom 250ms linear',
+      },
+    },
   },
   plugins: [],
 }
