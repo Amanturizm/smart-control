d0996319c0b975fce77ee3e446b6e8a5de44a88c
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2024-04-30 15:21:00 +0600

    Added logo

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: './src/script.js',
@@ -48,6 +49,14 @@ module.exports = {
     }),
     new MiniCssExtractPlugin({
       filename: 'style.css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: 'src/assets/images',
+          to: 'assets/images',
+        },
+      ],
     }),
   ],
 };
